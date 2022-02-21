a56145f51d91413fc67700e6b9f9ddd07aafee89
unknown <086mertege@gmail.com>
2022-02-21 13:24:22 +0100

    first commit

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    [SerializeField] float moveSpeed = 1f;
+    Rigidbody2D myRigidBody;
+
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        myRigidBody.velocity = new Vector2 (moveSpeed, 0f);
+    }
+
+    
+    // Sent when another object leaves a trigger collider attached to
+    // this object (2D physics only).
+    void OnTriggerExit2D(Collider2D other)
+    {
+        moveSpeed = -moveSpeed;
+        FlipEnemyFacing();
+    }
+
+    void FlipEnemyFacing()
+    {
+        transform.localScale = new Vector2 (-(Mathf.Sign(myRigidBody.velocity.x)), 1f);
+    }
+    
+}
